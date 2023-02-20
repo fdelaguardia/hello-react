266246269f653820e4c4471df30d969b3cfe479a
Fernand DelaGuardia <f.delaguardia@yahoo.com>
2023-02-20 15:00:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import icon1 from './images/icon1.png'
+import icon2 from './images/icon2.png'
+import icon3 from './images/icon3.png'
+import icon4 from './images/icon4.png'
+import logo from './images/ironhack-logo-xs.png'
+import menu from './images/menu-top-xs.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div className='body'>
+
+        <div className='nav'>
+          <img className='logo' src={logo} alt='logo'/>
+          <img className='menu' src={menu} alt='menu'/>
+        </div>
+
+        <div  className='main'>
+          <h1></h1>
+          <p></p>
+        </div>
+
+        <div className='button'>
+          <button>Awesome!</button>
+        </div>
+
+      </div>
+
+      <div className='images'>
+
+        <div>
+          <img />
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <img />
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <img />
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <img />
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+      
+      </div>
+
     </div>
   );
 }
